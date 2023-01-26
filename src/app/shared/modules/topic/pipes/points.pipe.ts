5ef4ec65837738e4729d1ebc33e0ab8850c7e927
Vlad <novskijvlad@gmail.com>
2023-01-26 22:41:23 +0200

    refactor(PointsPipe): shortened code

--- src/app/shared/modules/topic/pipes/points.pipe.ts
+++ src/app/shared/modules/topic/pipes/points.pipe.ts
@@ -1,10 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+// TODO: make it more generic
 @Pipe({
   name: 'points',
 })
 export class PointsPipe implements PipeTransform {
   transform(value: number): string {
-    return value === 1 ? value + ' point' : value + ' points';
+    return `${value} point${value !== 1 ? 's' : ''}`;
   }
 }
